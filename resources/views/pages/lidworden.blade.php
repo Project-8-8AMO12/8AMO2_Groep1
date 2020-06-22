84d654fd0c7c93eea771298ce702e4f04f2d7684
Jasper <jasperebeli@hotmail.nl>
2020-06-22 08:42:49 +0200

    Bijenstal, Lidworden, Activiteiten V1.2

--- resources/views/pages/lidworden.blade.php
+++ resources/views/pages/lidworden.blade.php
@@ -4,5 +4,33 @@
 
 
 @section("body")
-<h1>Lid worden</h1>
+        <div class="container rounded" style="margin: 50px auto 25px auto;">
+            <div class="row">
+                <div class="col-md-12 pt-2">
+                    <div class="col-md-12">
+                        <h1 class="text-warning pt-2 font-weight-bold">Lid worden van de vereniging</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="col-md-12">
+                        <p style="font-size: 1.6em;" class="text-white">Een lid van de Imkersvereniging Leiden en omstreken kan meedoen met de activiteiten van de vereniging (zoals imkersavonden), imkersbenodigdheden lenen (zoals slinger en waspers) en ontvangt de nieuwsbrieven. Ook heeft een lid inspraak d.m.v. stemrecht op de algemene leden vergadering. Lid worden van de Imkersvereniging Leiden en omstreken begint met lid worden van de Landelijke imkersvereniging NBV (Nederlandse Bijen Vereniging) met daarbij het lidmaatschap voor de afdeling Leiden. U kunt zich op de website van de NBV inschrijven:
+                            <br>
+                            <a style="color: gold" href="https://www.bijenhouders.nl/over-de-nbv/lidmaatschap#aanmeldformulier">https://www.bijenhouders.nl/over-de-nbv/lidmaatschap#aanmeldformulier</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container rounded" style="margin: 25px auto;">
+            <div class="row" style="">
+                <div class="col-md-12 m-2 pt-3 rounded" style="background-color: gold">
+                    <div class="col-md-12">
+                        <p class="" style="font-size: 1.6em;">Mocht u geen lid van de NBV en de afdeling Leiden willen worden, maar de vereniging en haar doelen wel een warm hart toedragen, dan kunt u vanaf 15,- euro per jaar ook donateur worden. U kunt dan meedoen met activiteiten, imkersbenodigdheden lenen en ontvangt nieuwsbrieven. U heeft geen inspraak als lid in de vereniging. Wilt u donateur worden, stuur dan een mail met uw naam, adres en telefoonnummer naar leidsebijen@live.nl en wij zorgen dat u donateur wordt.</></p></div>
+                </div>
+            </div>
+        </div>
 @stop
